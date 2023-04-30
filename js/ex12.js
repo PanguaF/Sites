2d65ffac48f309c7f1bf891f50e50f3ae1d93ecf
PanguaF <122706506+PanguaF@users.noreply.github.com>
2023-04-30 15:56:57 -0300

    Add files via upload

---
+++ js/ex12.js
@@ -0,0 +1,20 @@
+function carregar() {
+var msg = window.document.getElementById('carro')
+var img = window.document.getElementById('imagem') 
+var data = new Date()
+var hora = data.getHours()
+msg.innerText = `agora são ${hora} horas`
+if (hora >= 0 && hora < 12) {
+    // bom dia
+    img.src = 'img/manha.jpg'
+    document.body.style.background = '#231F14'
+} else if (hora >= 12 && hora <= 18) {
+    // boa tarde
+    img.src = 'img/tarde.jpg'
+    document.body.style.background = '##A0ADB6'
+} else {
+   // boa noite
+   img.src = 'img/noite.jpg'
+   document.body.style.background = '#202A11'
+}
+}
